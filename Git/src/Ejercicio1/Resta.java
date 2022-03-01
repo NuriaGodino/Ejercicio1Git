5e6fd25cccb4e525496ed6f857af9ccbf45e50ef
Nuria <nuria.godino@babelgroup.com>
2022-03-01 16:05:12 +0100

    Resta

---
+++ Git/src/Ejercicio1/Resta.java
@@ -0,0 +1,13 @@
+package Ejercicio1;
+
+public class Resta {
+private static Integer acum = 5;
+	
+	public static Integer restaDosNumeros(int a, int b) {
+		return a-b;
+	}
+	
+	public static Integer restaAcum(int ac) {
+		return ac - acum;
+	}
+}
