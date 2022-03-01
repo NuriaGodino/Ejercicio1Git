52bab968ca66f7fd6c869c8936013a4e14bb92f2
Nuria <nuria.godino@babelgroup.com>
2022-03-01 15:47:14 +0100

    Clase suma

---
+++ Git/src/Ejercicio1/Suma.java
@@ -0,0 +1,14 @@
+package Ejercicio1;
+
+public class Suma {
+	
+	private static Integer acum = 5;
+	
+	public static Integer sumaDosNumeros(int a, int b) {
+		return a+b;
+	}
+	
+	public static Integer sumaAcum(int ac) {
+		return ac + acum;
+	}
+}
